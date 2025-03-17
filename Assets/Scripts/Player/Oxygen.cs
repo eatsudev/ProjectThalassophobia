db35cfaf52336d90201f7d0cf90ef4f7f145c9e4
eatsudev <wizardbolt01@gmail.com>
2025-03-17 22:59:11 +0700

    oxygen

--- Assets/Scripts/Player/Oxygen.cs
+++ Assets/Scripts/Player/Oxygen.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Oxygen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float maxOxygen = 100f;
+    private float currentOxygen;
+    public float oxygenDepletionRate = 1f; 
+    public float depletionInterval = 10f;  
+
+    public bool hasOxygenTank = false;  
+
+    public Slider oxygenBar; 
+
+    private void Start()
     {
-        
+        currentOxygen = maxOxygen;
+        StartCoroutine(DecreaseOxygen());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator DecreaseOxygen()
     {
-        
+        while (currentOxygen > 0)
+        {
+            yield return new WaitForSeconds(depletionInterval);
+            currentOxygen -= oxygenDepletionRate;
+            currentOxygen = Mathf.Clamp(currentOxygen, 0, maxOxygen);
+            UpdateOxygenUI();
+
+            if (currentOxygen <= 0)
+            {
+                PlayerDrown(); 
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && hasOxygenTank)
+        {
+            ReloadOxygen();
+        }
+    }
+
+    private void ReloadOxygen()
+    {
+        currentOxygen = maxOxygen;
+        hasOxygenTank = false; 
+        Debug.Log("Oxygen Refilled");
+        UpdateOxygenUI();
+    }
+
+    private void UpdateOxygenUI()
+    {
+        if (oxygenBar != null)
+        {
+            oxygenBar.value = currentOxygen / maxOxygen;
+        }
+    }
+
+    private void PlayerDrown()
+    {
+        Debug.Log("Player has drowned");
+        //death logic
     }
 }
